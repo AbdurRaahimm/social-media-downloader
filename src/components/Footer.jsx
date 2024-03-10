68ccdee00b2021cb739c0b76682eca97ce2d3eda
Sayed Abdur Rahim <74426683+AbdurRaahimm@users.noreply.github.com>
2024-03-10 17:00:28 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white text-center p-4">
       <p className="text-sm">Made with ❤️ by 
-      <a href="https://www.linkedin.com/in/abhishek-kumar-singh-2a5b1a1b5/" className="text-blue-500"> Abdur Rahim</a>
+      <a href="https://www.facebook.com/Rahim72446" className="text-blue-500"> Abdur Rahim</a>
       </p>
     </footer>
   )
