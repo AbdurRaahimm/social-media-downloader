22837b95b2690076411ca1b560ac7bfecdd321dc
Sayed Abdur Rahim <74426683+AbdurRaahimm@users.noreply.github.com>
2025-06-06 08:59:47 +0000

    implement Microsoft clarity

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Clarity from '@microsoft/clarity';
+
+const projectId = "rv61d21r2n"
+
+Clarity.init(projectId);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
